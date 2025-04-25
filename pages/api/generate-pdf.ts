bdf5e615c27eb1786ff4929daefa395935d6259f
Julio Sieg <julio.sieg@gmail.com>
2025-04-24 23:46:32 -0300

    fix

--- pages/api/generate-pdf.ts
+++ pages/api/generate-pdf.ts
@@ -8,6 +8,7 @@ const s3 = new S3({
   secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
 })
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default async function handler(req: any, res: any) {
   if (req.method !== 'POST') return res.status(405).end()
 
