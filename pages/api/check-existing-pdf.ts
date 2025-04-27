5e9bd81e923ea4c17d39f95aac750b9d86640d24
Julio Sieg <julio.sieg@gmail.com>
2025-04-27 18:54:53 -0300

    try to fix

--- pages/api/check-existing-pdf.ts
+++ pages/api/check-existing-pdf.ts
@@ -8,33 +8,33 @@ const s3 = new S3({
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') return res.status(405).end()
-
+    console.dir('1a');
   const { session_id } = req.query
   if (!session_id) return res.status(400).json({ error: 'Missing session_id' })
-
+    console.dir('2b');
   const bucketName = process.env.NEXT_PUBLIC_S3_BUCKET_NAME
   const region = process.env.NEXT_PUBLIC_AWS_REGION
-
+  console.dir('3c');
   if (!bucketName || !region) {
     return res.status(500).json({ error: 'AWS S3 configuration is missing' })
   }
-
+  console.dir('4d');
   try {
     const fileName = `leitura-${session_id}.pdf`
     const params = {
       Bucket: bucketName,
       Key: fileName,
     }
-
+    console.dir('5e');
     // Verifica se o arquivo existe no S3
     const headData = await s3.headObject(params).promise()
-
+    console.dir('6f');
     if (headData) {
       // Se o arquivo existe, retorna o link público
       const pdfUrl = `https://${bucketName}.s3.${region}.amazonaws.com/${fileName}`
       return res.status(200).json({ pdfUrl })
     }
-
+    console.dir('7g');
     // Caso não exista
     return res.status(404).json({ error: 'PDF não encontrado' })
   } catch (error) {
--- pages/api/generate-pdf.ts
+++ pages/api/generate-pdf.ts
@@ -9,24 +9,25 @@ const s3 = new S3({
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default async function handler(req: any, res: any) {
+  console.dir('1');
   if (req.method !== 'POST') return res.status(405).end()
-
+    console.dir('2');
   const { html, session_id } = req.body
-
+  console.dir('3');
   if (!html) {
     return res.status(400).json({ error: 'HTML é obrigatório.' })
   }
-
+  console.dir('4');
   try {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.setContent(html, { waitUntil: 'networkidle0' })
-
+    console.dir('5');
     const pdfBuffer = await page.pdf({ format: 'A4' })
     await browser.close()
-
+    console.dir('6');
     const pdfName = `leitura-${session_id}.pdf`
-
+    console.dir('7');
     const params = {
       Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
       Key: `${pdfName}`,
@@ -34,11 +35,11 @@ export default async function handler(req: any, res: any) {
       ContentType: 'application/pdf',
       ACL: 'public-read', 
     }
-
+    console.dir('8');
     const data = await s3.upload(params).promise()
-
+    console.dir('9');
     const pdfUrl = data.Location
-
+    console.dir('10');
     return res.status(200).json({ pdfUrl })
   } catch (error) {
     console.error('Erro ao gerar PDF:', error)
