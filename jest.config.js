0485bc7750d16e3453a7592e21a12746b2589f1f
Julio Sieg <julio.sieg@gmail.com>
2025-06-28 15:07:51 -0300

    fix

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,15 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "pages/**/*.{ts,tsx,js,jsx}",
+    "!pages/_*.tsx",
+    "!**/node_modules/**",
+    "!**/vendor/**",
+  ],
+  coverageReporters: ["text-lcov", "lcov"],
   setupFiles: ["./jest.setup.js"],
   transform: {
     ...tsJestTransformCfg,
